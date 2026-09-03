af03bfbbbc667db03b60005e9647dcbe01faa67d
NicolasMussinhati <nicolas.rsantos@sptech.school>
2026-09-03 17:39:56 -0300

    Adicionando ambas as versões da calculadora e banco de dados

--- BancoPI1.sql
+++ BancoPI1.sql
@@ -1,5 +1,6 @@
-CREATE DATABASE monitoramento_vacinas;
-USE monitoramento_vacinas;
+CREATE DATABASE monitoramento_vacinas_nicolas;
+USE monitoramento_vacinas_nicolas;
+
 
 CREATE TABLE usuarios (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
@@ -10,20 +11,26 @@ CREATE TABLE usuarios (
     data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+
 CREATE TABLE temperaturas (
     id_temperatura INT AUTO_INCREMENT PRIMARY KEY,
     valor_temperatura DECIMAL(5,2) NOT NULL,
     data_hora DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Inserindo usuários cadastrados (com nomes baseados em animes)
 INSERT INTO usuarios (nome_completo, cpf, email, senha) VALUES
-('Ana Silva', '12345678901', 'ana.silva@email.com', '$2a$12$eImiTXuWVxfM37uY4JANjOL.eTYR.k7Yx1z3x.a4aG4q5g5a5a5a5'),
-('Carlos Eduardo', '98765432100', 'carlos.eduardo@email.com', '$2a$12$eImiTXuWVxfM37uY4JANjOL.eTYR.k7Yx1z3x.a4aG4q5g5a5a5a5'),
-('Mariana Costa', '45678912300', 'mariana.costa@email.com', '$2a$12$eImiTXuWVxfM37uY4JANjOL.eTYR.k7Yx1z3x.a4aG4q5g5a5a5a5');
+('Edward Elric', '11122233301', 'edward.elric@amestris.com', '$2a$12$eImiTXuWVxfM37uY4JANjOL.eTYR.k7Yx1z3x.a4aG4q5g5a5a5a5'),
+('Loid Forger', '22233344402', 'loid.forger@wise.gov', '$2a$12$eImiTXuWVxfM37uY4JANjOL.eTYR.k7Yx1z3x.a4aG4q5g5a5a5a5'),
+('Winry Rockbell', '33344455503', 'winry.rockbell@automail.com', '$2a$12$eImiTXuWVxfM37uY4JANjOL.eTYR.k7Yx1z3x.a4aG4q5g5a5a5a5'),
+('Shota Aizawa', '44455566604', 'aizawa.shota@ua.edu.br', '$2a$12$eImiTXuWVxfM37uY4JANjOL.eTYR.k7Yx1z3x.a4aG4q5g5a5a5a5');
 
 
 SELECT * FROM usuarios;
-UPDATE usuarios SET senha = '12345678' WHERE id_usuario BETWEEN 1 AND 3;
+
+
+UPDATE usuarios SET senha = '12345678' WHERE id_usuario BETWEEN 1 AND 4;
+
 
 INSERT INTO temperaturas (valor_temperatura, data_hora) VALUES
 (4.10, '2026-09-03 08:00:00'),
@@ -32,10 +39,12 @@ INSERT INTO temperaturas (valor_temperatura, data_hora) VALUES
 (9.20, '2026-09-03 11:00:00'),
 (3.50, '2026-09-03 12:00:00');
 
+
 SELECT * FROM temperaturas;
 
 
-INSERT INTO temperaturas (valor_temperatura, data_hora) VALUE
+INSERT INTO temperaturas (valor_temperatura, data_hora) VALUES
 (5.43, NOW());
+
 
 SELECT * FROM temperaturas;
